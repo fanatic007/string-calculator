cb9c2cb0cb4d2e436583a1c21bd309cfa31109e1
Sarang Tayde <sarang.tayde@gmail.com>
2024-08-19 22:23:06 +0530

    single number

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,4 +2,7 @@ export function add(input) {
   if(input?.trim()===''){
     return 0;
   }
+  if(!isNaN(input)){
+    return +input; 
+  }
 }
--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -3,3 +3,7 @@ import {add} from '.';
 test('empty string returns zero', () => {
   expect(add('')).toBe(0);
 });
+
+test('single number returns its value', () => {
+  expect(add('1')).toBe(1);
+});
